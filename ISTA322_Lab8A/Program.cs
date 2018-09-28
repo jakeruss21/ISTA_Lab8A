599772a5797189106e94dbcede2f7fe415083c8d
Jacob Russell <jacob.russell82@outlook.com>
2018-09-28 08:19:45 -0400

    Add project files.

---
+++ ISTA322_Lab8A/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTA322_Lab8A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
